024d9390f6c6e969ee9bfe989bfbb59f7e2910fb
Arturo Perez <perez.arturo2005@outlook.com>
2024-08-26 12:09:08 -0500

    826 notes

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+class Test {
+
+    public static void main(String[] args) {
+        System.out.print("success");
+    }
+
+
+    public static void main() {
+
+        System.out.println("Hello World!");
+        return 0;
+
+    }
+
+}
